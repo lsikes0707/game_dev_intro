55a8e8a243787c342c3dfb1b2d7300e958d87b56
Lacey Gruwell (Sikes) <lacey_sikes@my.cuesta.edu>
2023-09-07 21:28:51 -0700

    Pong-4: The Ball update
    Removed score display functionality, added new text to display at the top based on whether we are in play state or start state, ball now moves based on randomness and when we click 'enter/return', ball also starts in the same spot when in start state, and we've blocked the paddles from leaving the screen.

--- Pong/main.lua
+++ Pong/main.lua
@@ -3,8 +3,8 @@
 	GD50 2023
 	Pong Remake
 
-	pong-3
-	"The Paddle Update"
+	pong-4
+	"The Ball Update"
 
 	-- Main Program --
 
@@ -42,11 +42,12 @@ PADDLE_SPEED = 200
 function love.load()
  	love.graphics.setDefaultFilter('nearest', 'nearest')
 
+    -- "seed" the RNG so that calls to random are always random
+    -- use the current time, since that will vary on startup everytime
+    math.randomseed(os.time())
+
     -- more "retro-looking" font object we can use for any text
     smallFont = love.graphics.newFont('font.ttf', 8)
-
-    -- larger font for drawing the score on the screen
-    scoreFont = love.graphics.newFont('font.ttf', 32)
 
     -- set LOVE2D's active font to the smallFont object
     love.graphics.setFont(smallFont)
@@ -58,14 +59,22 @@ function love.load()
           vsync = true
       })
 
-      -- initialize score variables, used for rendering on the screen and
-      -- keeping track of the winner
-      player1Score = 0
-      player2Score = 0
-
       -- paddle positions on the Y axis (they can only move up and down)
       player1Y = 30
       player2Y = VIRTUAL_HEIGHT - 50
+
+      -- velocity and position variables for our ball when play starts
+      ballX = VIRTUAL_WIDTH / 2 - 2
+      ballY = VIRTUAL_HEIGHT / 2 - 2
+
+      -- math.random returns a random value between the left and right number
+      ballDX = math.random(2) == 1 and 100 or -100
+      ballDY = math.random(-50, 50)
+
+      -- game state variable used to transition between different parts
+      -- of the game (used for beginging, menus, main game, high score list,
+      -- etc) we will use this to determine behavior during render and update
+      gameState = 'start'
 end
 
 --[[
@@ -76,19 +85,31 @@ function love.update(dt)
     -- player 1 movement
     if love.keyboard.isDown('w') then
         -- add negative paddle speed to current Y scaled by deltaTime
-        player1Y = player1Y + -PADDLE_SPEED * dt    -- go up
+        -- now, we clamp our position between the bounds of the screen
+        -- math.max returns the greater of two values; 0 and player Y
+        -- will ensure we don't go above it
+        player1Y = math.max(0, player1Y + -PADDLE_SPEED * dt)    -- go up
     elseif love.keyboard.isDown('s') then
         -- add positive paddle speed to current Y scaled by deltaTime
-        player1Y = player1Y + PADDLE_SPEED * dt     -- go down
+        -- math.min returns the lesser of two values; bottom of the edge minus
+        -- paddle height and player Y will ensure we don't go below it
+        player1Y = math.min(VIRTUAL_HEIGHT - 20, player1Y + PADDLE_SPEED * dt)     -- go down
     end
 
     -- player 2 movement
     if love.keyboard.isDown('up') then
         -- add negative paddle speed to current Y scaled by deltaTime
-        player2Y = player2Y + -PADDLE_SPEED * dt    -- go up
+        player2Y = math.max(0, player2Y + -PADDLE_SPEED * dt)    -- go up
     elseif love.keyboard.isDown('down') then
         -- add positive paddle speed to current Y scaled by deltaTime
-        player2Y = player2Y  + PADDLE_SPEED * dt    -- go down
+        player2Y = math.min(VIRTUAL_HEIGHT - 20, player2Y  + PADDLE_SPEED * dt)    -- go down
+    end
+
+    -- update our ball based on its DX and DY only if we're in play state;
+    -- scale the velocity by dt so movement is framerate-independent
+    if gameState == 'play' then
+        ballX = ballX + ballDX * dt
+        ballY = ballY + ballDY * dt
     end
 end
 
@@ -101,6 +122,24 @@ function love.keypressed(key)
   if key == 'escape' then
     -- function LOVE gives us to terminate application
     love.event.quit()
+  -- if we press enter during the start state of the game, we'll go into
+  -- play mode, during play mode, the ball will move into a random direction
+  elseif key == 'enter' or key == 'return' then
+    if gameState == 'start' then
+        gameState = 'play'
+    else
+        gameState = 'start'
+
+        -- start ball's position in the middle of the screen
+        ballX = VIRTUAL_WIDTH / 2 - 2
+        ballY = VIRTUAL_HEIGHT / 2 - 2
+
+        -- given ball's x and y velocity a random starting value
+        -- the and/or pattern here is Lua's way of accomplishing a
+        -- ternary operation in other programming languages like C
+        ballDX = math.random(2) == 1 and 100 or -100
+        ballDY = math.random(-50, 50) * 1.5
+    end
   end
 end
 
@@ -118,20 +157,12 @@ function love.draw()
 
   -- draw welcome text toward the top of the screen
   love.graphics.setFont(smallFont)
-  love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
 
-  -- draw score on the left and right center of the screen
-  -- need to switch font to draw before actually printing
-  love.graphics.setFont(scoreFont)
-  love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50,
-        VIRTUAL_HEIGHT / 3)
-  love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30,
-        VIRTUAL_HEIGHT / 3)
-
-  -- 
-  -- paddles are simply rectangles we draw on the screen at certain points,
-  -- as is the ball
-  --
+  if gameState == 'start' then
+    love.graphics.printf('Hello Start State!', 0, 20, VIRTUAL_WIDTH, 'center')
+  else
+    love.graphics.printf('Hello Play State', 0, 20, VIRTUAL_WIDTH, 'center')
+  end
 
   -- render first paddle (left side) using players' Y variable
   love.graphics.rectangle('fill', 10, player1Y, 5, 20)
@@ -140,8 +171,7 @@ function love.draw()
   love.graphics.rectangle('fill', VIRTUAL_WIDTH -10, player2Y - 50, 5, 20)
 
   -- render ball (center)
-  love.graphics.rectangle('fill', VIRTUAL_WIDTH/ 2 - 2, VIRTUAL_HEIGHT / 2 - 2,
-  4, 4)
+  love.graphics.rectangle('fill', ballX, ballY, 4, 4)
 
   -- end rendering at virtual resolution
   push:apply('end')
