ecf006b68d1d183faeee77cc386e49bc946967aa
Lacey Gruwell (Sikes) <lacey_sikes@my.cuesta.edu>
2023-09-12 23:44:43 -0700

    Pong-11: Audio Update
    Added sound table, play calls throughout main and the sound files to their own sounds directory.

--- Pong/main.lua
+++ Pong/main.lua
@@ -3,8 +3,8 @@
 	GD50 2023
 	Pong Remake
 
-	pong-10
-	"The Victory Update"
+	pong-11
+	"The Audio Update"
 
 	-- Main Program --
 
@@ -73,6 +73,14 @@ function love.load()
     scoreFont = love.graphics.newFont('font.ttf', 32)
     love.graphics.setFont(smallFont)
 
+    -- set up our sounds effects; later, we can just index this table and
+    -- call each entry's 'play' method
+    sounds = {
+        ['paddle_hit'] = love.audio.newSource('sounds/paddle_hit.wav', 'static'),
+        ['score'] = love.audio.newSource('sounds/score.wav', 'static'),
+        ['wall_hit'] = love.audio.newSource('sounds/wall_hit.wav', 'static')
+    }
+
 	-- initialize our virtual resolution
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
           fullscreen = false,
@@ -124,6 +132,8 @@ function love.update(dt)
             else
                 ball.dy = math.random(10, 100)
             end
+
+            sounds['paddle_hit']:play()
         end
         if ball:collides(player2) then
             ball.dx = -ball.dx * 1.03
@@ -135,18 +145,22 @@ function love.update(dt)
             else
                 ball.dy = math.random(10, 100)
             end
+
+            sounds['paddle_hit']:play()
         end
 
         -- detect upper and lower screen boundary collision and reverse if collided
         if ball.y <= 0 then
             ball.y = 0
             ball.dy = -ball.dy
+            sounds['wall_hit']:play()
         end
 
         -- -4 to account for the ball's size
         if ball.y >= VIRTUAL_HEIGHT - 4 then
             ball.y = VIRTUAL_HEIGHT - 4
             ball.dy = -ball.dy
+            sounds['wall_hit']:play()
         end
 
         -- if we reach the left or right edge of the screen,
@@ -154,6 +168,7 @@ function love.update(dt)
         if ball.x < 0 then
             servingPlayer = 1
             player2Score = player2Score + 1
+            sounds['score']:play()
 
             -- if we've reached a score of 10, the game is over; set the
             -- state to done so we can show the victory message
@@ -170,6 +185,7 @@ function love.update(dt)
         if ball.x > VIRTUAL_WIDTH then
             servingPlayer = 2
             player1Score = player1Score + 1
+            sounds['score']:play()
 
             if player1Score == 10 then
                 winningPlayer = 1
