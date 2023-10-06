82c8882f2fd67a230e8d97e987675dc011f0cdfe
Lacey Gruwell (Sikes) <lacey_sikes@my.cuesta.edu>
2023-10-05 19:46:27 -0700

    Breakout
    Adding Breakout completed without additions.

---
+++ breakout/src/constants.lua
@@ -0,0 +1,22 @@
+--[[
+    GD50 2023
+    Breakout Remake
+
+    -- constants --
+
+    Author: Lacey Gruwell
+    gruwell.lacey
+
+    Some global constants for our application.
+]]
+
+-- size of our actual window
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+-- size we're trying to emulate with push
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+-- paddle movement speed
+PADDLE_SPEED = 200
