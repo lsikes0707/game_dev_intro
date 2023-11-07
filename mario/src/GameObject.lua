8d3f6883dba6c62c69caef95968bf15c91f70073
Lacey Gruwell (Sikes) <lacey_sikes@my.cuesta.edu>
2023-11-07 12:02:26 -0800

    Adding Mario and Zelda
    Adding Mario as is with large gap bug and reverted zelda to original while a debug locally.

---
+++ mario/src/GameObject.lua
@@ -0,0 +1,42 @@
+--[[
+    GD50
+    -- Super Mario Bros. Remake --
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+]]
+
+GameObject = Class{}
+
+function GameObject:init(def)
+    self.x = def.x
+    self.y = def.y
+    self.animation = def.animation or nil
+    self.animationFrameOffset = 0
+    self.texture = def.texture
+    self.width = def.width
+    self.height = def.height
+    self.frame = def.frame
+    self.solid = def.solid
+    self.collidable = def.collidable
+    self.consumable = def.consumable
+    self.onCollide = def.onCollide
+    self.onConsume = def.onConsume
+    self.hit = def.hit
+end
+
+function GameObject:collides(target)
+    return not (target.x > self.x + self.width or self.x > target.x + target.width or
+            target.y > self.y + self.height or self.y > target.y + target.height)
+end
+
+function GameObject:update(dt)
+    if self.animation then
+        self.animation:update(dt)
+        self.animationFrameOffset = self.animation:getCurrentFrame()
+    end
+end
+
+function GameObject:render()
+    love.graphics.draw(gTextures[self.texture], gFrames[self.texture][self.frame + self.animationFrameOffset], self.x, self.y)
+end
